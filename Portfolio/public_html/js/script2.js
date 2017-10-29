8280db87f644cafb564467243c3681686e17b81f
mikerhinos <mikerhinosfr@gmail.com>
2017-10-29 14:31:40 +0100

    Terminé. La calculatrice a été mal conçue niveau responsive, il faudrait la refaire entièrement, tant pis ce n'était pas le sujet du tp.

--- Portfolio/public_html/js/script2.js
+++ Portfolio/public_html/js/script2.js
@@ -2,18 +2,36 @@ var container = $('.contenu');
 var calc = 'calc/index.html';
 var accueil = 'contAcc.html';
 var game = 'game/testNiveau.html';
+var cv = 'cv.html';
+var contact = 'contact.html';
+var valide = 'valid.html';
 
 $('#calc').click( function(e) {
     e.preventDefault();
-    container.load(calc); //'<object type="text/html" data="calc/index.html" ></object>';
+    container.load(calc);
     return false; } );
 
 $('.accueil').click( function(e) {
     e.preventDefault();
-    container.load(accueil); //'<object type="text/html" data="calc/index.html" ></object>';
+    container.load(accueil);
     return false; } );
 
 $('#game').click( function(e) {
     e.preventDefault();
-    container.load(game); //'<object type="text/html" data="calc/index.html" ></object>';
+    container.load(game);
+    return false; } );
+
+$('#cv').click( function(e) {
+    e.preventDefault();
+    container.load(cv);
+    return false; } );
+
+$('#contact').click( function(e) {
+    e.preventDefault();
+    container.load(contact);
+    return false; } );
+
+$('#formu').click( function(e) {
+    e.preventDefault();
+    container.load(valide);
     return false; } );
